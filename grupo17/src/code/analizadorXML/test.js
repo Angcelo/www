f0f07e82d09b1e591282f63f161363302ecdef47
Angcelo <anmaramaga@gmail.com>
2021-06-07 12:52:08 -0600

    Elimine cosas que nada que ver

--- grupo17/src/code/analizadorXML/test.js
+++
@@ -1,8 +0,0 @@
-var parser = require('./grammar')
-var helpers = require('./helpers')
-
-
-var conteindo = helpers.getContentByFile('test1.xml')
-var resultado = parser.parse(conteindo)
-console.log('Resultado')
-console.log(JSON.stringify(resultado))
