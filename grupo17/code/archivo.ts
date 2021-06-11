ace330722fb7fdf40b4d2fca5d202911db1461c7
Jorge Ambrocio Ventura <53419722+JorgeAmbrocio@users.noreply.github.com>
2021-06-11 14:49:03 -0600

    cambios test 1

--- grupo17/code/archivo.ts
+++ grupo17/code/archivo.ts
@@ -4,6 +4,11 @@ export default class archivo{
         console.log("Hola mundo");
         console.log("nuevoCambio");
         console.log("nuevo cambio de nuevo");
+
+        console.log('David estuvo aquí :D');
+        console.log('David estuvo aquí :D');
+        console.log('David estuvo aquí :D');
+        
     }
 
 }
